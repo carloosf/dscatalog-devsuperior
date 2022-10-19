519d25acf4b7c2cf09ec3cac05c70be896d39038
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2022-10-19 00:27:26 -0300

    att

--- frontend/js/front/home.js
+++ frontend/js/front/home.js
@@ -8,16 +8,44 @@ console.log(window.pageYOffset)
 window.addEventListener("scroll", upScroll)
 
 function upScroll() {
-    if (window.pageYOffset > 0) {
+    if (window.pageYOffset > 300) {
 
-        header.classList.add("header-logo-fixed")
-        header.classList.remove("header-logo")
+        header.style.position = 'fixed'
+        header.style.background = '#008C8C'
 
-    } else if (window.pageYOffset < 20) {
+    } else if (window.pageYOffset < 100) {
 
-
-        header.classList.remove("header-logo-fixed")
-        header.classList.add("header-logo")
+        header.style.position = 'absolute'
     }
 };
+
+//DROPDOWN
+
+
+const SideBar = {
+    bar: document.querySelector('#dropdown-menu'),
+    menu: document.querySelector('#menu'),
+    closeIcon: document.querySelector('#close'),
+    turmaNome: document.querySelector('#turma-nome'),
+
+    open: function () {
+        SideBar.bar.style.width = '250px';
+        SideBar.turmaNome.style.left = '40%';
+    },
+
+    close: function () {
+        SideBar.bar.style.width = '0';
+        SideBar.turmaNome.style.left = '12%';
+    },
+
+    init: function () {
+        this.menu.addEventListener('click', this.open);
+        this.closeIcon.addEventListener('click', this.close);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    SideBar.init();
+})
+
 
--- frontend/js/front/painel.js
+++ frontend/js/front/painel.js
@@ -1,4 +1,3 @@
-
 //SideBar Animação//
 
 const SideBar = {
@@ -7,19 +6,19 @@ const SideBar = {
   closeIcon: document.querySelector('#close'),
   turmaNome: document.querySelector('#turma-nome'),
 
-  open: function() {
+  open_sidebar: function () {
     SideBar.bar.style.width = '250px';
     SideBar.turmaNome.style.left = '40%';
   },
 
-  close: function() {
+  close_sidebar: function () {
     SideBar.bar.style.width = '0';
-    SideBar.turmaNome.style.left = '12%';
-  }, 
+    SideBar.turmaNome.style.left = '7%';
+  },
 
-  init: function() {
-    this.menu.addEventListener('click', this.open);
-    this.closeIcon.addEventListener('click', this.close);
+  init: function () {
+    this.menu.addEventListener('click', this.open_sidebar);
+    this.closeIcon.addEventListener('click', this.close_sidebar);
   }
 }
 
@@ -28,4 +27,41 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 //CADASTRAR ALUNO//
+
+const alunos = document.querySelectorAll('#aluno')
+const contador = document.querySelector('#qtd-alunos')
+var qtdAlunos = alunos.length
+renderContador()
+
+function addContador() {
+  qtdAlunos++
+  renderContador()
+}
+
+function renderContador() {
+  document.querySelector('#qtd-alunos').innerHTML = qtdAlunos
+}
+
+function addAluno() {
+  var table = document.querySelector(".tabela-aluno")
+  qtdAlunos + alunos.length
+
+  var row = table.insertRow(qtdAlunos + 1);
+
+  var id = row.insertCell(0);
+  var matricula = row.insertCell(1);
+  var nome = row.insertCell(2);
+  var idade = row.insertCell(3);
+  var media = row.insertCell(4);
+  var Status = row.insertCell(5);
+
+  id.innerHTML = qtdAlunos + 1;
+
+  nome.innerHTML = document.querySelector('#nome').value;
+  matricula.innerHTML = document.querySelector('#matricula').value;
+  idade.innerHTML = document.querySelector('#idade').value;
+  media.innerHTML = 0
+  Status.innerHTML = document.querySelector('#status').value;
+  addContador()
+}
 
